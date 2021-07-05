485648f1ad1656509c61302d5b092a4f98a7cb96
lucas silva <lucassporte_12@hotmail.com>
2021-07-05 10:58:06 -0300

    aula 2

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-const nav = document.querySelector('#header nav');
-const toggle = document.querySelectorAll('nav .toggle');
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+const links = document.querySelectorAll('nav ul li a')
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
-for(const element of toggle){
-    element.addEventListener('click', function(){
-        nav.classList.toggle('show');
-    })
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
 }
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
