8c050fbdb8dd300e43d2cfcc90e70e089fe7b7f4
lucas silva <lucassporte_12@hotmail.com>
2021-07-05 17:57:39 -0300

    aula3

--- main.js
+++ main.js
@@ -23,3 +23,12 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+// Carrosel do depoimento
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
