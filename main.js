10221f06df3cac923db2270e189214f61f72ae4c
lucas silva <lucassporte_12@hotmail.com>
2021-07-06 11:36:59 -0300

    aula3

--- main.js
+++ main.js
@@ -32,3 +32,20 @@ const swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true
 })
+// mostrar elementos quando da scroll na pagina
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .text, #home .image
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+`,
+  { interval: 100 }
+)
