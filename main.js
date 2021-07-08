78784d2bffe9f5cd4ef761949557482526b9c858
lucas silva <lucassporte_12@hotmail.com>
2021-07-08 17:35:44 -0300

    aula4.1

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 const links = document.querySelectorAll('nav ul li a')
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
@@ -16,13 +14,17 @@ for (const link of links) {
   })
 }
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
+
 // Carrosel do depoimento
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
@@ -45,7 +47,23 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
-`,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
   { interval: 100 }
 )
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
